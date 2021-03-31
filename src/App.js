213dd7d24cef14df6ffeb5ce61f9ee2bf3234ef9
daniel mamani <dani.oscar.257@gmail.com>
2021-03-31 13:08:45 -0300

    OK: firts commit, swa filemanager frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { FileManager, FileNavigator } from '@opuscapita/react-filemanager';
+import connectorNodeV1 from '@opuscapita/react-filemanager-connector-node-v1';
+
+const apiOptions = {
+  ...connectorNodeV1.apiOptions,
+  apiRoot: `http://localhost:3020` // Or you local Server Node V1 installation.
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: '480px' }}>
+      <FileManager>
+        <FileNavigator
+          id="filemanager-1"
+          api={connectorNodeV1.api}
+          apiOptions={apiOptions}
+          capabilities={connectorNodeV1.capabilities}
+          listViewLayout={connectorNodeV1.listViewLayout}
+          viewLayoutOptions={connectorNodeV1.viewLayoutOptions}
+        />
+      </FileManager>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +13,31 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { FileManager, FileNavigator } from '@opuscapita/react-filemanager';
+// import connectorNodeV1 from '@opuscapita/react-filemanager-connector-node-v1';
+
+// const apiOptions = {
+//   ...connectorNodeV1.apiOptions,
+//   apiRoot: `http://localhost:3020` // Or you local Server Node V1 installation.
+// }
+
+// const fileManager = (
+//  <div style={{ height: '480px' }}>
+//     <FileManager>
+//       <FileNavigator
+//         id="filemanager-1"
+//         api={connectorNodeV1.api}
+//         apiOptions={apiOptions}
+//         capabilities={connectorNodeV1.capabilities}
+//         listViewLayout={connectorNodeV1.listViewLayout}
+//         viewLayoutOptions={connectorNodeV1.viewLayoutOptions}
+//       />
+//     </FileManager>
+//   </div>
+// );
+
+// ReactDOM.render(fileManager, document.body);
