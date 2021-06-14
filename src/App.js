ad0c48bd5c651571e2f2cffa64556e920ecb3fb4
daniel mamani <dani.oscar.257@gmail.com>
2021-06-14 01:07:04 -0300

    OK: import custom filemanager node connector

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FileManager, FileNavigator } from '@opuscapita/react-filemanager';
-import connectorNodeV1 from '@opuscapita/react-filemanager-connector-node-v1';
+import connectorNodeV1 from 'react-filemanager-connector-node-v1';
 
 //S: server and files cfg
 var ServerUrl= window.location.href.match(/(https?:\/\/[^\/]+)/)[0]; //A: tomar protocolo, servidor y puerto de donde esta esta pagina
